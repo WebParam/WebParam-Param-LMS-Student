f0f1130a68ce53b944b1ea240f5fe68badbc51ca
simondev-net <simon.mamabolo@fraktionaldev.com>
2024-10-22 14:34:37 +0200

    deleted service worker to resolve conflicts

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,6 @@ export default function RootLayout({
                 </CourseIdProvider>
               </Provider>
               <BootstrapClient />
-
               <ToastContainer />
             </LessonProvider>
           </UserProvider>
